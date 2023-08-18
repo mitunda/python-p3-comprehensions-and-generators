e6b4cdc531de5e35b5a2848f5bd63606418adad7
mitunda <peterbrian28@gmail.com>
2023-08-18 17:34:26 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    even_numbers = [n for n in sequence if n % 2 == 0]
+    return even_numbers
+
+# Example usage
+result = return_evens([0, 1, 3, 5, 7, 8, 9])
+print(result)  # Prints the list of even elements
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_sentence = [n+ "!" for n in sentence_list]
+    return exclaimed_sentence
